684e48a0e9b3e6c49b72a418bc4d47170be53d2c
cicciopanzer27 <jechov.heyg@gmail.com>
2025-12-28 02:27:49 +0100

    Fix lakefile.lean: Specify Lean 4.9.0 version and use stable mathlib4 tag for compatibility

--- lakefile.lean
+++ lakefile.lean
@@ -3,9 +3,10 @@ open Lake DSL
 
 package «borel-iut» where
   version := "0.1.0"
+  leanVersion := "4.9.0"
   dependencies := #[{
     name := "mathlib",
-    src := Source.git "https://github.com/leanprover-community/mathlib4" "master"
+    src := Source.git "https://github.com/leanprover-community/mathlib4" (rev := "v4.9.0")
   }]
 
 @[default_target]
